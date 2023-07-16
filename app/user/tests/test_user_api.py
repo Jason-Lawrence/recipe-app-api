27fc8643ac0aacf732deb1c2f7eb8d3debde259f
Jason <thejasonlawrence1@gmail.com>
2023-07-16 11:06:42 -0400

    Fix linting errors

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -10,6 +10,7 @@ CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
 
+
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
@@ -52,7 +53,9 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+            ).exists()
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
@@ -129,9 +132,7 @@ class PrivateUserApiTests(TestCase):
         payload = {'name': 'Updated name', 'password': 'newpasswor123d'}
 
         res = self.client.patch(ME_URL, payload)
-
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        
